6a9248d8f58b39aa403cedd794bcd1006a18c1ac
wodelu <1058744843@qq.com>
2018-08-08 18:12:40 +0800

    药品列表页样式修改

--- app/modules/pharbers/v1/hospital/route.js
+++ app/modules/pharbers/v1/hospital/route.js
@@ -1,12 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-	setupController(controller, model) {
-		this._super(controller, model);
-		controller.queryHospList(model.uuid);
-		controller.queryBudget(model.uuid);
-		controller.queryManpower(model.uuid);
-	},
+	// setupController(controller, model) {
+	// 	this._super(controller, model);
+	// 	controller.queryHospList(model.uuid);
+	// 	controller.queryBudget(model.uuid);
+	// 	controller.queryManpower(model.uuid);
+	// },
 	actions: {
 		hospDetail(hospid) {
 			this.transitionTo('pharbers.v1.hosp-detail',
