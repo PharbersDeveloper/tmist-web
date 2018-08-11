e0e40418f44de55191656c77dc83d79204919370
Faiz <891366402@qq.com>
2018-08-11 23:14:18 +0800

    新增pharbers-tm-decision

--- app/modules/pharbers/v1/controller.js
+++ app/modules/pharbers/v1/controller.js
@@ -6,101 +6,171 @@ export default Controller.extend({
     isShowList: true,
     init() {
         this.repList = {
-            "id": "thisisAllRepre",
-            "title": "成员能力概况",
-            "reps": [{
-                "id": "thisisxiaosong",
-                "avatar": "/assets/images/a1.jpeg",
-                "name": '小赵',
-                "self": ['28岁', '研究生', '英语专业', '3年工作经验', '入职一年'],
-                "square": [{
-                    "title": "业务经验",
-                    "value": ["心血管类", "抗生素类"]
-                }, {
-                    "title": "优势",
-                    "value": ["客情关系", "学习能力", "幻灯演讲"]
-                }, {
-                    "title": "代提高",
-                    "value": ["产品知识", "科室会", "专业性的拜访"]
-                }],
-                "scores": [{
-                    "title": "销售技巧",
-                    "score": 45,
-                }, {
-                    "title": "产品知识",
-                    "score": 56,
-                }, {
-                    "title": "工作积极性",
-                    "score": 65,
-                }],
-                "showinfo": [{
-                    "key": "总能力值",
-                    "value": 45,
-                }, {
-                    "key": "产品知识",
-                    "value": 54,
-                }, {
-                    "key": "销售技巧",
-                    "value": 32,
-                }, {
-                    "key": "工作积极性",
-                    "value": 21,
-                }]
-            }, {
-                "id": "this-is-bigzhang",
-                "avatar": "/assets/images/a2.jpeg",
-                "name": '小钱',
-                "self": ['38岁', '研究生', '英语专业', '8年工作经验', '入职八年'],
-                "square": [{
-                    "title": "业务经验",
-                    "value": ["心血管类", "抗生素类"]
-                }, {
-                    "title": "优势",
-                    "value": ["客情关系", "学习能力", "幻灯演讲"]
-                }, {
-                    "title": "代提高",
-                    "value": ["产品知识", "科室会", "专业性的拜访"]
-                }],
-                "scores": [{
-                    "title": "销售技巧",
-                    "score": 56,
-                }, {
-                    "title": "产品知识",
-                    "score": 68,
-                }, {
-                    "title": "工作积极性",
-                    "score": 76,
-                }],
-                "showinfo": [{
-                    "key": "总能力值",
-                    "value": 56,
-                }, {
-                    "key": "产品知识",
-                    "value": 65,
-                }, {
-                    "key": "销售技巧",
-                    "value": 43,
-                }, {
-                    "key": "工作积极性",
-                    "value": 36,
-                }]
-            }],
-            "detail": {}
+            "id": "暂定UUID",
+            "component_data": [
+                {
+                    "key": "target-representative-card",
+                    "values": [
+                        {
+                            "id": "izteqaf57vb14mop",
+                            "name": "小赵",
+                            "ability": [
+                                {
+                                    "title": "总能力值",
+                                    "value": 45
+                                },
+                                {
+                                    "title": "产品知识",
+                                    "value": 54
+                                },
+                                {
+                                    "title": "销售技巧",
+                                    "value": 32
+                                },
+                                {
+                                    "title": "工作积极性",
+                                    "value": 21
+                                }
+                            ],
+                            "rep_image": "https://pharbers-images.oss-cn-beijing.aliyuncs.com/pharbers-tm-decision-ember-addon/a1.jpeg"
+                        },
+                        {
+                            "id": "izteqaf57vb14mob",
+                            "name": "小钱",
+                            "ability": [
+                                {
+                                    "title": "总能力值",
+                                    "value": 45
+                                },
+                                {
+                                    "title": "产品知识",
+                                    "value": 54
+                                },
+                                {
+                                    "title": "销售技巧",
+                                    "value": 32
+                                },
+                                {
+                                    "title": "工作积极性",
+                                    "value": 21
+                                }
+                            ],
+                            "rep_image": "https://pharbers-images.oss-cn-beijing.aliyuncs.com/pharbers-tm-decision-ember-addon/a2.jpeg"
+                        }
+                    ]
+                },
+                {
+                    "key": "target-rep-detail",
+                    "values": [
+                        {
+                            "id": "izteqaf57vb14mop",
+                            "lable": {
+                                "name": "小赵",
+                                "rep_image": "https://pharbers-images.oss-cn-beijing.aliyuncs.com/pharbers-tm-decision-ember-addon/a1.jpeg",
+                                "age": 28,
+                                "education": "研究生",
+                                "specialities": "英语专业",
+                                "working_hours": 8,
+                                "entry_hours": 1
+                            },
+                            "self_business": [
+                                {
+                                    "title": "业务经验",
+                                    "value": [
+                                        "心血管类",
+                                        "抗生素类"
+                                    ]
+                                },
+                                {
+                                    "title": "优势",
+                                    "value": [
+                                        "客情关系",
+                                        "学习能力",
+                                        "幻灯演讲"
+                                    ]
+                                },
+                                {
+                                    "title": "代提高",
+                                    "value": [
+                                        "产品知识",
+                                        "科室会",
+                                        "专业性的拜访"
+                                    ]
+                                }
+                            ],
+                            "score": [
+                                {
+                                    "title": "销售技巧",
+                                    "score": 45
+                                },
+                                {
+                                    "title": "产品知识",
+                                    "score": 56
+                                },
+                                {
+                                    "title": "工作积极性",
+                                    "score": 65
+                                }
+                            ]
+                        },
+                        {
+                            "id": "izteqaf57vb14mob",
+                            "lable": {
+                                "name": "小钱",
+                                "rep_image": "https://pharbers-images.oss-cn-beijing.aliyuncs.com/pharbers-tm-decision-ember-addon/a2.jpeg",
+                                "age": 28,
+                                "education": "研究生",
+                                "specialities": "英语专业",
+                                "working_hours": 8,
+                                "entry_hours": 1
+                            },
+                            "self_business": [
+                                {
+                                    "title": "业务经验",
+                                    "value": [
+                                        "心血管类",
+                                        "抗生素类"
+                                    ]
+                                },
+                                {
+                                    "title": "优势",
+                                    "value": [
+                                        "客情关系",
+                                        "学习能力",
+                                        "幻灯演讲"
+                                    ]
+                                },
+                                {
+                                    "title": "代提高",
+                                    "value": [
+                                        "产品知识",
+                                        "科室会",
+                                        "专业性的拜访"
+                                    ]
+                                }
+                            ],
+                            "score": [
+                                {
+                                    "title": "销售技巧",
+                                    "score": 45
+                                },
+                                {
+                                    "title": "产品知识",
+                                    "score": 56
+                                },
+                                {
+                                    "title": "工作积极性",
+                                    "score": 65
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
         };
     },
     actions: {
-        showDetail(id) {
-            this.set('isShowList', false);
-            this.get('repList.reps').forEach((ele) => {
-                if (ele.id === id) {
-                    this.set('repList.detail', ele);
-                }
-            })
-        },
-        hideDetail() {
-            this.set('isShowList', true);
-        },
-        backPrev() {
+        close() {
             this.get('decisionInfo').hide()
         }
     }
