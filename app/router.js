f839f738845deb62792cbb1b29d5a788835026af
Frank <wsgoodman@163.com>
2018-08-20 16:53:30 +0800

    新建 decision 路由，重新设计逻辑

--- app/router.js
+++ app/router.js
@@ -11,16 +11,17 @@ Router.map(function() {
     this.route('pharbers', function() {
         this.route('v1', function() {
             this.route('index', { path: '/:uuid' }, function() {
-                this.route('hosp-list', { path: "hosp-list" });
-                this.route('hosp-list-detail', { path: "hosp-list-detail/:hospid" });
+              this.route('hosp-list', { path: "hosp-list" });
+              this.route('hosp-list-detail', { path: "hosp-list-detail/:hospid" });
 
-                this.route('reports', function() {
-                    this.route('hosp-product', { path: "hosp-product/:id" });
-                    this.route('represent-product', { path: "represent-product/:id" });
-                    this.route('resource', { path: "resource/:id" });
-                    this.route('represent-target', { path: "represent-target/:id" });
-                    this.route('represent-ability', { path: "represent-ability/:id" });
-                });
+              this.route('reports', function() {
+                  this.route('hosp-product', { path: "hosp-product/:id" });
+                  this.route('represent-product', { path: "represent-product/:id" });
+                  this.route('resource', { path: "resource/:id" });
+                  this.route('represent-target', { path: "represent-target/:id" });
+                  this.route('represent-ability', { path: "represent-ability/:id" });
+              });
+              this.route('decision');
             });
             this.route('imitate-train');
             this.route('evaluation');
---
+++ tests/unit/modules/pharbers/v1/index/decision/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:pharbers/v1/index/decision', 'Unit | Route | pharbers/v1/index/decision', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
