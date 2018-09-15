5886b5d3999c0e060e39b4820a5587e8237abef5
Frank <wsgoodman@163.com>
2018-09-15 18:46:36 +0800

    为runCalc 做准备。

--- app/modules/pharbers/v1/imitate-train/decision/controller.js
+++ app/modules/pharbers/v1/imitate-train/decision/controller.js
@@ -11,7 +11,6 @@ export default Controller.extend({
         runCalc() {
             let repRecord = this.store.peekAll('repinputinfo');
             let manRecord = this.store.peekAll('managerinputinfo').firstObject;
-            manRecord._internalModel.__relationships = { initializedRelationships: {} };
             let hospRecord = this.store.peekAll('hospitalbaseinfo');
             let hospRecordJsonApi = this.store.object2JsonApi('hospitalbaseinfo', hospRecord);
             let manRecordJsonApi = this.store.object2JsonApi('managerinputinfo', manRecord);
@@ -32,14 +31,14 @@ export default Controller.extend({
             }))
             let conditions = this.store.object2JsonApi('request', req);
             let cache = this.get('store').peekAll('medicsnotice')
-            if( cache.length == 0 ) {
+            if (cache.length == 0) {
                 this.store.queryObject('/api/v1/medicsnotices/0', 'medicsnotice', conditions).then((mwithns) => {
                     component.set('data', mwithns);
                 });
             } else {
-                component.set('data', cache.firstObject); 
+                component.set('data', cache.firstObject);
             }
-            
+
         },
 
         getHospInfo(component) {
@@ -57,7 +56,7 @@ export default Controller.extend({
             let loadHospitallocalStorage = function(data) {
                 data.forEach(elem => {
                     let cache = localStorage.getItem('hospinput_' + that.get('uuid') + "_" + elem.id);
-                    if( cache ) {
+                    if (cache) {
                         that.store.pushPayload(JSON.parse(cache))
                     }
                 })
@@ -65,7 +64,7 @@ export default Controller.extend({
 
             let hospitalCache = this.get('store').peekAll('hospitalbaseinfo')
             let repInputCache = this.get('store').peekAll('repinputinfo')
-            if ( hospitalCache.length == 0 || repInputCache.length == 0) {
+            if (hospitalCache.length == 0 || repInputCache.length == 0) {
                 new RSVP.all([
                     this.store.queryMultipleObject('/api/v1/hospitalinfo/0', 'hospitalbaseinfo', conditions),
                     this.store.queryMultipleObject('/api/v1/repinputcards/0', 'repinputinfo', conditions)
@@ -74,7 +73,7 @@ export default Controller.extend({
                     component.set('totalRep', data[1]);
                     loadHospitallocalStorage(component.get('data'));
                 })
-    
+
             } else {
                 component.set('data', hospitalCache);
                 component.set('totalRep', repInputCache);
@@ -82,9 +81,9 @@ export default Controller.extend({
             }
 
         },
-        
+
         totalBugdetRatio(totalBudget) {
-            this.get('totalBudgetRatio').set('budget', totalBudget); 
+            this.get('totalBudgetRatio').set('budget', totalBudget);
         },
 
         getRepBudget(component) {
@@ -100,19 +99,19 @@ export default Controller.extend({
             let repInputCache = this.get('store').peekAll('repinputinfo')
             let managerInputCache = this.get('store').peekAll('managerinputinfo')
 
-            if( repInputCache.length == 0 || managerInputCache.length == 0) {
+            if (repInputCache.length == 0 || managerInputCache.length == 0) {
                 new RSVP.all([
                     this.store.queryMultipleObject('/api/v1/repinputcards/0', 'repinputinfo', conditions),
                     this.store.queryObject('/api/v1/managerInputInfo/0', 'managerinputinfo', conditions)
                 ]).then(function(data) {
                     component.set('data', data[0])
                     component.set('mdata', data[1])
-                }) 
+                })
             } else {
                 component.set('data', repInputCache)
-                component.set('mdata', managerInputCache.firstObject) 
+                component.set('mdata', managerInputCache.firstObject)
             }
-            this.set('totalBudgetRatio', component); 
+            this.set('totalBudgetRatio', component);
         },
 
         getManagerTime(component) {
@@ -148,32 +147,22 @@ export default Controller.extend({
                 val: this.get('uuid'),
             }))
             let conditions = this.store.object2JsonApi('request', req);
-            
-            let cache = this.get('store').peekAll('repinputinfo')
 
-            if ( cache.length == 0 ) {
+            let cache = this.get('store').peekAll('repinputinfo');
+
+            if (cache.length == 0) {
                 this.store.queryMultipleObject('/api/v1/repinputcards/0', 'repinputinfo', conditions).then((rinfo) => {
                     component.set('repList', rinfo);
-                }); 
+                });
             } else {
-                component.set('repList', cache); 
+                component.set('repList', cache);
             }
 
         },
 
-        recoveryHospInput(hospid) {
-        },
 
-        hospInput(id) {
-        },
 
-        chooseRep(repid) {
-        },
 
-        repInput(rid) {
-        },
 
-        getManagerTime(component) {
-        }
     },
 });
