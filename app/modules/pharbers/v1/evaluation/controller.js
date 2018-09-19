fed8abc4eb9b39e9c00e7a61d9c08383cbd1427e
Frank <wsgoodman@163.com>
2018-09-19 20:40:08 +0800

    删除无效路由。

--- app/modules/pharbers/v1/evaluation/controller.js
+++ app/modules/pharbers/v1/evaluation/controller.js
@@ -15,14 +15,6 @@ export default Controller.extend({
     },
     actions: {
         getRadarData(component) {
-            // let req = this.store.createRecord('request', {
-            //     res: 'assess_report',
-            // });
-            // req.get('eqcond').pushObject(this.store.createRecord('eqcond', {
-            //     key: 'id',
-            //     val: '5b9f952fe53d3735f0007497',
-            // }))
-            // let conditions = this.store.object2JsonApi('request', req);
             let cache = this.get('store').peekAll('evaluation_radar_data')
             if (cache.length == 0) {
                 this.store.queryObject('/api/v1/evaluationRadar/0', 'evaluation_radar_data', this.queryConditions())
@@ -32,41 +24,21 @@ export default Controller.extend({
             } else {
                 component.set('radarData', cache.firstObject);
             }
-
         },
 
         getCardData(component) {
-            // let req = this.store.createRecord('request', {
-            //     res: 'assess_report',
-            // });
-            // req.get('eqcond').pushObject(this.store.createRecord('eqcond', {
-            //     key: 'id',
-            //     val: '5b9f952fe53d3735f0007497',
-            // }))
-            // let conditions = this.store.object2JsonApi('request', req);
-
             // let cache = this.get('store').peekAll('evaluation_card_data');
             // if (cache.length == 0) {
             this.store.queryMultipleObject('/api/v1/evaluationCards/0', 'evaluation_card_data', this.queryConditions())
                 .then((cdata) => {
-                    console.log(cdata);
-                    console.log("===========");
                     component.set('cardData', cdata);
                 });
             // } else {
             // component.set('cardData', cache.firstObject);
             // }
-
         },
+
         getLineData(component) {
-            // let req = this.store.createRecord('request', {
-            //     res: 'assess_report',
-            // });
-            // req.get('eqcond').pushObject(this.store.createRecord('eqcond', {
-            //     key: 'id',
-            //     val: '5b9f952fe53d3735f0007497',
-            // }))
-            // let conditions = this.store.object2JsonApi('request', req);
             let cache = this.get('store').peekAll('evaluation_line_data')
             if (cache.length == 0) {
                 this.store.queryObject('/api/v1/evaluationLine/0', 'evaluation_line_data', this.queryConditions())
@@ -76,7 +48,6 @@ export default Controller.extend({
             } else {
                 component.set('lineData', cache.firstObject);
             }
-
         },
     }
 });
--- app/router.js
+++ app/router.js
@@ -10,18 +10,6 @@ Router.map(function() {
     this.route('page-not-found', { path: '/*path' });
     this.route('pharbers', function() {
         this.route('v1', function() {
-            this.route('index', { path: '/:uuid' }, function() {
-                this.route('hosp-list', { path: "hosp-list" });
-                this.route('hosp-list-detail', { path: "hosp-list-detail/:hospid" });
-                this.route('reports', function() {
-                    this.route('hosp-product', { path: "hosp-product/:id" });
-                    this.route('represent-product', { path: "represent-product/:id" });
-                    this.route('resource', { path: "resource/:id" });
-                    this.route('represent-target', { path: "represent-target/:id" });
-                    this.route('represent-ability', { path: "represent-ability/:id" });
-                });
-                this.route('decision');
-            });
             this.route('imitate-train', function() {
                 this.route('decision', { path: "decision/:uuid" });
             });
